51e5caa93b3d9afae50b55ff5941fa2eaf1df0a9
antoine.maille64 <antoine.maille64@gmail.com>
2024-06-10 12:19:21 +0000

    typo

--- src/main/java/com/formation/AI/controller/CocktailController.java
+++ src/main/java/com/formation/AI/controller/CocktailController.java
@@ -1,7 +1,11 @@
 package com.formation.AI.controller;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-public class CoktailController {
+public class CocktailController {
+
+
+    
 
 }
