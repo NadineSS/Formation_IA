27ba372f516252b722f643478df6909fb9a71edc
antoine.maille64 <antoine.maille64@gmail.com>
2024-06-11 08:40:57 +0000

    fix bug

--- src/main/java/com/formation/AI/repository/CocktailRepository.java
+++ src/main/java/com/formation/AI/repository/CocktailRepository.java
@@ -1,15 +1,21 @@
 package com.formation.AI.repository;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import java.util.Arrays;
 import java.util.List;
+
 import com.formation.AI.model.CocktailModel;
 
+import lombok.extern.slf4j.Slf4j;
+
+
 @Repository
+@Slf4j
 public class CocktailRepository {
 
     @Value("${api.url}")
@@ -28,17 +34,24 @@ public class CocktailRepository {
      */
     public List<CocktailModel> getCocktails() {
 
-        // Make the API call and get the response.
-        ResponseEntity<CocktailApiResponse> response = restTemplate.getForEntity(
-                apiUrl,
-                CocktailApiResponse.class);
+        try {
 
-        // Check if the API call was successful.
-        if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
-            // Return the list of cocktails.
-            return Arrays.asList(response.getBody().getDrinks());
-        } else {
-            // Return null if there is an error.
+            // Make the API call and get the response.
+            ResponseEntity<CocktailApiResponse> response = restTemplate.getForEntity(
+                    apiUrl + apiUrlAllCocktails,
+                    CocktailApiResponse.class);
+
+            // Check if the API call was successful.
+            if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
+                // Return the list of cocktails.
+                return Arrays.asList(response.getBody().getDrinks());
+            } else {
+                // Return null if there is an error.
+                return null;
+            }
+        } catch (HttpClientErrorException  e) {
+            log.error("Error fetching cocktails: {} - {}", e.getStatusCode(), e.getMessage());
+            e.printStackTrace();
             return null;
         }
     }
