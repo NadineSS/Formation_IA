6b829f44dba5c761e89e4114460667e6799917e5
antoine.maille64 <antoine.maille64@gmail.com>
2024-06-28 13:38:26 +0000

    ex2 Simplification

--- src/main/java/com/formation/AI/controller/CocktailController.java
+++ src/main/java/com/formation/AI/controller/CocktailController.java
@@ -1,16 +1,25 @@
 package com.formation.AI.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.formation.AI.model.CocktailModel;
+import com.formation.AI.service.CocktailService;
 
 
 @RestController
 public class CocktailController {
 
+    @Autowired
+    private CocktailService cocktailService;
+
     @GetMapping("/cocktail")
-    public ResponseEntity<String> getAllCocktails() {
-        return ResponseEntity.ok("ok");
+    public ResponseEntity<List<CocktailModel>> getAllCocktails() {
+        return ResponseEntity.ok(this.cocktailService.getAllCocktails());
     }
 
 }
--- src/main/java/com/formation/AI/repository/CocktailRepository.java
+++ src/main/java/com/formation/AI/repository/CocktailRepository.java
@@ -1,9 +1,22 @@
 package com.formation.AI.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
 
 
 @Repository
 public class CocktailRepository {
+
+    
+    @Value("${api.url}")
+    private String apiUrl;
+
+    @Value("${api.url.allCocktails}")
+    private String apiUrlAllCocktails;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
 }
